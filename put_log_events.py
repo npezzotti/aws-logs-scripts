5a0ff9ac3cd1ce29dca5111a4a460177b92666df
npezzotti <nathan.pezzotti@datadoghq.com>
2021-04-10 13:30:31 -0400

    refactored error handling logic

--- put_log_events.py
+++ put_log_events.py
@@ -4,22 +4,20 @@ import sys
 from time import time
 import boto3
 
-def main(args):
-    usage = "To run this script, supply a log group and stream name as required command line arguments."
-    
+USAGE = "To run this script, supply a log group and stream name as required command line arguments."
+
+def main(args):    
     if (len(args) != 2):
-        return print(usage) 
+        return print(USAGE)
 
     client = boto3.client('logs')
 
     log_group = args[0]
     log_stream = args[1]
-
-    sequenceToken = get_sequence_token(client, log_group, log_stream)
-
     timestamp = int(time() * 1000)
-
+    
     try:
+        sequence_token = get_sequence_token(client, log_group, log_stream)
         response = client.put_log_events(
             logGroupName=log_group,
             logStreamName=log_stream,
@@ -29,27 +27,24 @@ def main(args):
                     'message': 'ERROR'
                 }
             ],
-            sequenceToken=sequenceToken
+            sequenceToken=sequence_token
         )
-    except client.exceptions.InvalidSequenceTokenException as e: 
-        return print(f'Incorrect sequence token: {str(e)}')
-    except client.exceptions.UnrecognizedClientException as e:
-        return print(f'Please configure your AWS CLI before running thsi script: https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html')
+    except Exception as err:
+        return print(err)
 
-    return print("OK")
+    return print(response)
 
 def get_sequence_token(client, log_group, log_stream):
-    try:
-        response = client.describe_log_streams(
-            logGroupName=log_group,
-            logStreamNamePrefix=log_stream, 
-            limit=1
-        )
-    except client.exceptions.ResourceNotFoundException as e:
-        return print(f'Resource not found: {str(e)}')
-
+    
+    response = client.describe_log_streams(
+        logGroupName=log_group,
+        logStreamNamePrefix=log_stream, 
+        limit=1
+    )
+    
     if (len(response.get('logStreams')) == 0):
-        return print(f"Log stream \"{log_stream}\" does not exist in log group \"{log_group}\".")
+        raise Exception(f"Log stream \"{log_stream}\" does not exist in log group \"{log_group}\".")
+
     sequence_token = response.get('logStreams')[0].get('uploadSequenceToken')
     
     return sequence_token
